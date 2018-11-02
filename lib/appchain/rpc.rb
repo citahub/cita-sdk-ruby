1e77734d6a11ca5bb146ff63547009b7835b4551
classicalliu <classicalliu@gmail.com>
2018-11-02 16:20:22 +0800

    Update to adapt CITA 0.20

--- lib/appchain/rpc.rb
+++ lib/appchain/rpc.rb
@@ -70,8 +70,14 @@ module AppChain
     # @return [Hash]
     def transfer(to:, private_key:, value:, quota: 30_000)
       valid_until_block = block_number["result"].hex + 88
-      chain_id = get_meta_data("latest").dig "result", "chainId"
-      transaction = Transaction.new(nonce: Utils.nonce, valid_until_block: valid_until_block, chain_id: chain_id, to: to, value: value, quota: quota)
+      meta_data = get_meta_data("latest")["result"]
+      version = meta_data["version"]
+      chain_id = if version.zero?
+                   meta_data["chainId"]
+                 elsif version == 1
+                   meta_data["chainIdV1"]
+                 end
+      transaction = Transaction.new(nonce: Utils.nonce, valid_until_block: valid_until_block, chain_id: chain_id, to: to, value: value, quota: quota, version: version)
       send_transaction(transaction, private_key)
     end
   end
